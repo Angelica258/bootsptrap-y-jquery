fb01965bc21bc118b72f6bf51d11549e6a4e9fb7
Angelik Bermudez <32436970+Angelica258@users.noreply.github.com>
2018-10-01 23:10:22 -0500

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,9 @@
 $("#selh3").click(function(event){
 	$("#contenedor").load('paginas/colombia.html');
 });
+$("#selh4").click(function(event){
+	$("#contenedor").load('index.html');
+});
+$("#selh5").click(function(event){
+	$("#contenedor").load('paginas/contacto.html');
+});
